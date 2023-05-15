7fe67d4cdb7ec5a8b473c152d09c80f4018d1b83
Momin Rather <momin.rather@gmail.com>
2023-05-15 12:04:13 +0530

    Weather App added default city

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 function showWeather() {
-  const city = document.getElementById("search").value;
-  const apiKey = "9e45232019b6a180894b47481d9db2f5";
+  let city = "";
+  if (document.getElementById("search").value.length != 0) {
+    city = document.getElementById("search").value;
+  } else city = "Jammu";
+
+  // let city = document.getElementById("search").value;
+  // if (city == "") city = "Jammu";
+
+  alert(city);
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6c0387614d2116469f8a6bfac2817576&units=metric`;
 
@@ -25,3 +32,5 @@ function showWeather() {
 
   document.getElementById("weather").style.display = "flex";
 }
+
+window.onload = showWeather();
